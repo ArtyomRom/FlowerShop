c1ba97e9a9227b6e2c1586c3f4eeda15193dda6f
anyukurilets <anyu.kutilez@mail.ru>
2025-03-26 15:56:26 +0300

    sorting bouquets by prices and occasions

--- flowershop/telegram_bot/staticfiles/handlers.py
+++ flowershop/telegram_bot/staticfiles/handlers.py
@@ -1,4 +1,5 @@
 import os
+
 from aiogram import Router, types
 from aiogram.filters import CommandStart
 from aiogram.types import FSInputFile, InlineKeyboardMarkup, InlineKeyboardButton
@@ -15,6 +16,7 @@ router = Router()
 class CustomOccasionState(StatesGroup):
     waiting_for_custom_occasion = State()
     waiting_for_phone = State()
+    waiting_for_price = State()  # Новый шаг
 
 
 class OrderState(StatesGroup):
@@ -46,9 +48,139 @@ async def handle_other_occasion(callback: types.CallbackQuery, state: FSMContext
 async def process_custom_occasion(message: types.Message, state: FSMContext):
     user_occasion = message.text
     text = f"Спасибо! Вы указали повод: *{user_occasion}* 💐"
-    keyboard = keyboards.get_occasion_keyboard()
+    keyboard = keyboards.get_help_keyboard()
     await message.answer(text, parse_mode="Markdown", reply_markup=keyboard)
     await state.clear()
+
+
+@router.callback_query(lambda c: c.data == "consultation")
+async def request_consultation(callback: types.CallbackQuery, state: FSMContext):
+    await callback.message.answer(
+        "Пожалуйста, введите ваш номер телефона и наш администратор свяжется с вами в течение 20 минут"
+    )
+    await state.set_state(CustomOccasionState.waiting_for_phone)
+    await callback.answer()
+
+
+@router.message(CustomOccasionState.waiting_for_phone)
+async def process_phone(message: types.Message, state: FSMContext):
+    phone_number = message.text
+    await message.answer(
+        f"Спасибо! Ваш номер {phone_number} принят. Ожидайте звонка от администратора."
+    )
+    await state.clear()
+
+
+@router.callback_query(lambda c: c.data.startswith("occasion_"))
+async def handle_occasion(callback: types.CallbackQuery, state: FSMContext):
+    """Обработчик для выбора стандартных поводов"""
+    occasion_key = callback.data.replace("occasion_", "")
+    bouquets = await sync_to_async(list)(Bouquet.objects.filter(occasion=occasion_key))
+
+    if not bouquets:
+        await callback.message.answer(
+            "К сожалению, у нас пока нет букетов для этого случая 😔"
+        )
+        return
+
+    # Запрос на выбор суммы
+    await callback.message.answer(
+        "На какую сумму рассчитываете?", reply_markup=keyboards.get_select_price()
+    )
+    await state.set_state(CustomOccasionState.waiting_for_price)
+    await state.update_data(occasion=occasion_key)
+    await callback.answer()
+
+
+@router.callback_query(lambda c: c.data.startswith("price_"))
+async def handle_price_selection(callback: types.CallbackQuery, state: FSMContext):
+    """Обработчик для выбора цены"""
+    price_key = callback.data.replace("price_", "")
+
+    # Получаем повод из состояния
+    user_data = await state.get_data()
+    user_occasion = user_data.get("occasion")
+
+    # Получаем букеты, которые соответствуют выбранному поводу и цене
+    bouquets = await sync_to_async(list)(
+        Bouquet.objects.filter(occasion=user_occasion, price__lte=price_key)
+    )
+
+    if not bouquets:
+        await callback.message.answer(
+            f"К сожалению, нет букетов для повода {user_occasion} в этом диапазоне 😔"
+        )
+        return
+
+    # Отправка подходящих букетов
+    for bouquet in bouquets:
+        text = (
+            f"🌸 *{bouquet.name}*\n{bouquet.description}\n💰 Цена: {bouquet.price} руб."
+        )
+        keyboard = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(
+                        text="✅ Выбрать этот букет",
+                        callback_data=f"bouquet_{bouquet.id}",
+                    )
+                ]
+            ]
+        )
+        if bouquet.image:
+            image_path = bouquet.image.path
+            if os.path.exists(image_path):
+                img_input = FSInputFile(image_path)
+                await callback.message.answer_photo(
+                    img_input,
+                    caption=text,
+                    parse_mode="Markdown",
+                    reply_markup=keyboard,
+                )
+            else:
+                await callback.message.answer(
+                    f"Изображение для букета {bouquet.name} не найдено!"
+                )
+        else:
+            await callback.message.answer(
+                text, parse_mode="Markdown", reply_markup=keyboard
+            )
+
+    await callback.answer()
+
+
+@router.callback_query(lambda c: c.data.startswith("bouquet_"))
+async def handle_bouquet_selection(callback: types.CallbackQuery):
+    """Обработчик для выбора букета"""
+    bouquet_id = int(callback.data.replace("bouquet_", ""))
+    try:
+        bouquet = await sync_to_async(Bouquet.objects.get)(id=bouquet_id)
+    except Bouquet.DoesNotExist:
+        await callback.message.answer("Этот букет больше недоступен 😔")
+        return
+
+    text = f"🎉 Вы выбрали букет *{bouquet.name}*! 💐\n💰 Цена: {bouquet.price} руб."
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="🛒 Оформить заказ", callback_data=f"order_{bouquet.id}"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="🌸 Заказать консультацию", callback_data="consultation"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    text="📚 Посмотреть всю коллекцию", callback_data="view_collection"
+                )
+            ],
+        ]
+    )
+    await callback.message.answer(text, parse_mode="Markdown", reply_markup=keyboard)
+    await callback.answer()
 
 
 @router.callback_query(lambda c: c.data == "view_collection")
@@ -92,135 +224,86 @@ async def view_collection(callback: types.CallbackQuery):
     await callback.answer()
 
 
-@router.callback_query(lambda c: c.data == "consultation")
-async def request_consultation(callback: types.CallbackQuery, state: FSMContext):
-    await callback.message.answer(
-        "Пожалуйста, введите ваш номер телефона и наш администратор свяжется с вами в течение 20 минут"
-    )
-    await state.set_state(CustomOccasionState.waiting_for_phone)
-    await callback.answer()
-
-
-@router.message(CustomOccasionState.waiting_for_phone)
-async def process_phone(message: types.Message, state: FSMContext):
-    phone_number = message.text
-    await message.answer(
-        f"Спасибо! Ваш номер {phone_number} принят. Ожидайте звонка от администратора."
-    )
-    await state.clear()
-
-
-@router.callback_query(lambda c: c.data.startswith("occasion_"))
-async def handle_occasion(callback: types.CallbackQuery):
-    occasion_key = callback.data.replace("occasion_", "")
-    if occasion_key == "other":
-        return
-    bouquets = await sync_to_async(list)(Bouquet.objects.filter(occasion=occasion_key))
-    if not bouquets:
-        await callback.message.answer(
-            "К сожалению, у нас пока нет букетов для этого случая 😔"
-        )
-        return
-    for bouquet in bouquets:
-        text = (
-            f"🌸 *{bouquet.name}*\n{bouquet.description}\n💰 Цена: {bouquet.price} руб."
-        )
-        keyboard = InlineKeyboardMarkup(
-            inline_keyboard=[
-                [
-                    InlineKeyboardButton(
-                        text="✅ Выбрать этот букет",
-                        callback_data=f"bouquet_{bouquet.id}",
-                    )
-                ]
-            ]
-        )
-        if bouquet.image:
-            image_path = bouquet.image.path
-            if os.path.exists(image_path):
-                img_input = FSInputFile(image_path)
-                await callback.message.answer_photo(
-                    img_input,
-                    caption=text,
-                    parse_mode="Markdown",
-                    reply_markup=keyboard,
-                )
-            else:
-                await callback.message.answer(
-                    f"Изображение для букета {bouquet.name} не найдено!"
-                )
-        else:
-            await callback.message.answer(
-                text, parse_mode="Markdown", reply_markup=keyboard
-            )
-    await callback.answer()
-
-
-@router.callback_query(lambda c: c.data.startswith("bouquet_"))
-async def handle_bouquet_selection(callback: types.CallbackQuery):
-    bouquet_id = int(callback.data.replace("bouquet_", ""))
-    try:
-        bouquet = await sync_to_async(Bouquet.objects.get)(id=bouquet_id)
-    except Bouquet.DoesNotExist:
-        await callback.message.answer("Этот букет больше недоступен 😔")
-        return
-
-    text = f"🎉 Вы выбрали букет *{bouquet.name}*! 💐\n💰 Цена: {bouquet.price} руб."
-    keyboard = keyboards.get_bouquet_keyboard(bouquet)
-    await callback.message.answer(text, parse_mode="Markdown", reply_markup=keyboard)
-    await callback.answer()
-
-
+# Обработчик начала оформления заказа
 @router.callback_query(lambda c: c.data.startswith("order_"))
 async def start_order(callback: types.CallbackQuery, state: FSMContext):
+    """Обработчик начала оформления заказа"""
+
     bouquet_id = int(callback.data.replace("order_", ""))
+
+    # Получаем пользователя по Telegram ID
     user, created = await sync_to_async(Customer.objects.get_or_create)(
         tg_id=callback.from_user.id
     )
+
+    # Сохраняем букет в данные пользователя
     selected_bouquet = await sync_to_async(Bouquet.objects.get)(id=bouquet_id)
+
+    # Сохраняем данные в FSM
     await state.update_data(user_id=user.id, bouquet_id=selected_bouquet.id)
 
+    # Запрашиваем имя
     await callback.message.answer("Введите ваше имя:")
     await state.set_state(OrderState.waiting_for_name)
+
     await callback.answer()
 
 
+# Обработчик ввода имени
 @router.message(OrderState.waiting_for_name)
 async def process_name(message: types.Message, state: FSMContext):
+    """Сохранение имени и запрос адреса"""
     await sync_to_async(Customer.objects.filter(tg_id=message.from_user.id).update)(
         name=message.text
     )
+
     await message.answer("Спасибо! Теперь введите ваш адрес для доставки:")
     await state.set_state(OrderState.waiting_for_address)
 
 
+# Обработчик ввода адреса
 @router.message(OrderState.waiting_for_address)
 async def process_address(message: types.Message, state: FSMContext):
+    """Сохранение адреса и запрос даты/времени"""
     await state.update_data(address=message.text)
+
     await message.answer(
         "Спасибо! Теперь введите дату и время доставки(в формате YYYY-MM-DD HH:MM):"
     )
     await state.set_state(OrderState.waiting_for_delivery_time)
 
 
+# Обработчик ввода даты и времени доставки
 @router.message(OrderState.waiting_for_delivery_time)
 async def process_delivery_time(message: types.Message, state: FSMContext):
+    """Сохранение даты/времени и запрос номера телефона"""
+    # Парсим дату и время
     parsed_date = dateparser.parse(message.text, languages=["ru"])
+
+    # Если дата не распознана, то выводим ошибку
     if not parsed_date:
         await message.answer(
             "Вы некорректно ввели данные. Пожалуйста, введите дату и время доставки(в формате YYYY-MM-DD HH:MM)"
         )
         return
+
+    # Сохраняем дату/время в состоянии
     await state.update_data(delivery_time=parsed_date)
+
     await message.answer("Спасибо! Теперь введите ваш номер телефона:")
     await state.set_state(OrderState.waiting_for_phone)
 
 
+# Обработчик ввода номера телефона и создание заказа
 @router.message(OrderState.waiting_for_phone)
 async def process_phone(message: types.Message, state: FSMContext):
+    """Сохранение номера телефона и финальное подтверждение заказа"""
     user_data = await state.get_data()
+
     user = await sync_to_async(Customer.objects.get)(id=user_data["user_id"])
     bouquet = await sync_to_async(Bouquet.objects.get)(id=user_data["bouquet_id"])
+
+    # Создание заказа
     order = await sync_to_async(Order.objects.create)(
         customer=user,
         bouquet=bouquet,
@@ -228,8 +311,9 @@ async def process_phone(message: types.Message, state: FSMContext):
         delivery_time=user_data["delivery_time"],
         status="new",
     )
+
+    # Подтверждение заказа
     await message.answer(
-        f"✅ Ваш заказ оформлен!\n💐 Букет: {bouquet.name}\n📍 Адрес: {user_data['address']}\n"
-        f"⏳ Дата и время: {user_data['delivery_time']}\n📞 Телефон: {message.text}"
+        f"✅ Ваш заказ оформлен!\n💐 Букет: {bouquet.name}\n📦 Адрес: {user_data['address']}\n🕒 Время доставки: {user_data['delivery_time']}\n📱 Телефон: {message.text}"
     )
     await state.clear()
--- flowershop/telegram_bot/staticfiles/keyboards.py
+++ flowershop/telegram_bot/staticfiles/keyboards.py
@@ -1,13 +1,13 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-# Основные кнопки
-inline_kb = InlineKeyboardMarkup(
-    inline_keyboard=[
-        [InlineKeyboardButton(text="Выбрать букет", callback_data="choose_flower")],
-        [InlineKeyboardButton(text="Связаться с флористом", callback_data="contact_florist")],
-    ]
-)
 
+def get_help_keyboard():
+    buttons=[
+        [InlineKeyboardButton(text="🌸 Заказать консультацию", callback_data="consultation")],
+        [InlineKeyboardButton(text="📚 Посмотреть всю коллекцию", callback_data="view_collection")],]
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
+
+     
 
 def get_occasion_keyboard():
     """Генерирует inline-клавиатуру с поводами"""
@@ -21,37 +21,13 @@ def get_occasion_keyboard():
     return InlineKeyboardMarkup(inline_keyboard=buttons)
 
 
-def get_bouquet_keyboard(bouquet):
-    """Генерирует inline-клавиатуру для выбранного букета"""
-    return InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text="🛒 Оформить заказ", callback_data=f"order_{bouquet.id}"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    text="🌸 Заказать консультацию", callback_data="consultation"
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    text="📚 Посмотреть всю коллекцию", callback_data="view_collection"
-                )
-            ],
-        ]
-    )
-
-
 def get_select_price():
     """Генерирует inline-клавиатуру с выбором цены"""
     buttons = [
-        [InlineKeyboardButton(text="~500 руб.", callback_data="price_500")],
-        [InlineKeyboardButton(text="~1000 руб.", callback_data="price_1000")],
         [InlineKeyboardButton(text="~2000 руб.", callback_data="price_2000")],
-        [InlineKeyboardButton(text="Больше", callback_data="price_more")],
-        [InlineKeyboardButton(text="Не важно", callback_data="price_any")],
+        [InlineKeyboardButton(text="~3000 руб.", callback_data="price_3000")],
+        [InlineKeyboardButton(text="~4000 руб.", callback_data="price_4000")],
+        [InlineKeyboardButton(text="~5000 руб.", callback_data="price_5000")],
     ]
 
     return InlineKeyboardMarkup(inline_keyboard=buttons)
