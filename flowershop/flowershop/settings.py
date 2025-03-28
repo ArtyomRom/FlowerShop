fdf347060be3ec63b9be6f680f456f5821d576eb
Artyom <zik21b@gmail.com>
2025-03-28 22:20:26 +0300

    Поменял настройки для БД

--- flowershop/flowershop/settings.py
+++ flowershop/flowershop/settings.py
@@ -16,9 +16,9 @@ import dj_database_url
 
 PORT = os.getenv("PORT", "10000")
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-}
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+# }
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,12 +83,12 @@ WSGI_APPLICATION = "flowershop.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 # Password validation
