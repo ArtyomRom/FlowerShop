3d0b55d8f9d59520cbce113c8d0e88b83e545a12
Artyom <zik21b@gmail.com>
2025-03-29 19:27:35 +0300

    Перенес бд на Postgresql

--- flowershop/flowershop/settings.py
+++ flowershop/flowershop/settings.py
@@ -83,13 +83,22 @@ WSGI_APPLICATION = "flowershop.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'flower_shop_database',
+        'USER': 'flower_shop_admin',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
