56a0ce629aec89655382b4a758f8090913043b80
Artyom <zik21b@gmail.com>
2025-03-30 18:45:31 +0300

    Изменил urls

--- flowershop/flowershop/urls.py
+++ flowershop/flowershop/urls.py
@@ -19,12 +19,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from shop import views
+
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Главная страница работает!")
 
 urlpatterns = [
+    path('', home, name='home'),
     path("admin/", admin.site.urls),
     path('api/', include('shop.urls')),
-    path('', views.index),
 ]
 
 
